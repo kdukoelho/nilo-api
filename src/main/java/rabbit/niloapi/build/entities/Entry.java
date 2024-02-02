4023673f243ce3df05212c6c95319acb3db04a17
kdukoelho <ce.figueiredoc@gmail.com>
2024-02-02 13:59:44 -0300

    feat: database configuration

---
+++ src/main/java/rabbit/niloapi/build/entities/Entry.java
@@ -0,0 +1,42 @@
+package rabbit.niloapi.build.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity(name = User.ENTITY_NAME) @Table(name = Entry.TABLE_NAME)
+@RequiredArgsConstructor @NoArgsConstructor(force = true)
+@Data
+public class Entry {
+    public static final String TABLE_NAME = "tb_entries";
+    public static final String ENTITY_NAME = "entry";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true)
+    private String id;
+
+    @NotBlank
+    @Column(name="title", unique = true, length = 30)
+    @Size(max=30)
+    private final String title;
+
+    @Column(name = "url", length = 120)
+    @Size(min = 0, max = 120)
+    private final String url;
+
+    @Column(name = "login", length = 80)
+    @Size(max = 80)
+    private final String login;
+
+    @Column(name = "password", length = 100)
+    @Size(max = 100)
+    private final String password;
+
+    @ManyToOne
+    private User user;
+
+}
