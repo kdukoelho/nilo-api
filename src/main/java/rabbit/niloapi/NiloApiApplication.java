2dcc757794194f5e10b2d985a17b4c798484aa3c
kdukoelho <ce.figueiredoc@gmail.com>
2024-02-02 13:48:28 -0300

    initial commit

---
+++ src/main/java/rabbit/niloapi/NiloApiApplication.java
@@ -0,0 +1,13 @@
+package rabbit.niloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rabbit/niloapi/NiloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rabbit.niloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
