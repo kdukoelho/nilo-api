2ece7784b4bb6d91dc0a4ef78d08d4f0fc0ada04
kdukoelho <ce.figueiredoc@gmail.com>
2024-02-16 12:57:19 -0300

    feat: user role

--- src/main/java/rabbit/niloapi/model/user/User.java
+++ src/main/java/rabbit/niloapi/model/user/User.java
@@ -4,15 +4,18 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import rabbit.niloapi.dto.user.UserRequestDTO;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity(name = User.ENTITY_NAME) @Table(name = User.TABLE_NAME)
 @RequiredArgsConstructor
 @Data
-public class User {
+public class User implements UserDetails {
     public static final String TABLE_NAME = "tb_users";
     public static final String ENTITY_NAME = "user";
 
@@ -35,4 +38,34 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private List<Entry> entriesList = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.passwordHash;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/rabbit/niloapi/model/user/UserRole.java
@@ -0,0 +1,13 @@
+package rabbit.niloapi.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String role;
+}
