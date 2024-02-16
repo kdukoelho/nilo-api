a780ce0b6870b55c692d31fb23e3e5807de9cd45
kdukoelho <ce.figueiredoc@gmail.com>
2024-02-16 13:08:56 -0300

    feat: find by username

--- src/main/java/rabbit/niloapi/repository/UserRepository.java
+++ src/main/java/rabbit/niloapi/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package rabbit.niloapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import rabbit.niloapi.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import rabbit.niloapi.model.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+UserDetails findByUsername(String username);
+
 }
