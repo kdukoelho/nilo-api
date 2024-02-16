9ac4b5231f03ca7de1249f8fde01b31a8a95e50e
kdukoelho <ce.figueiredoc@gmail.com>
2024-02-16 12:57:49 -0300

    feat: authorization service

---
+++ src/main/java/rabbit/niloapi/service/AuthorizationService.java
@@ -0,0 +1,18 @@
+package rabbit.niloapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import rabbit.niloapi.repository.UserRepository;
+
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
